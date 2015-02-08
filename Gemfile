c9013cfa15c6a2fa8ad838fdb5e12da2f2867a7f
eavee15 <evillalona111@gmail.com>
2015-02-07 22:11:02 -0500

    added devise and added flash notices

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'turbolinks'
 
 gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
+gem 'devise', '~> 3.4.1.rc2'
 
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  #note to set to domain name
+  config.action_mailer.default_url_options = { host: 'eavee-pinteresting.com'}
+
 end
